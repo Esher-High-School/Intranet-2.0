34e3b1bb9e661e408a9ccce094b68fa36773bbd5
Joel Nichols <me@joelnichols.co.uk>
2015-05-18 10:56:52 +0100

    Removed un-necessary Json

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,5 @@
 class Page < ActiveRecord::Base
+  extend FriendlyId
+  validates :title, presence: true
+  friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518093734) do
+ActiveRecord::Schema.define(version: 20150518095427) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
+
+  add_index "pages", ["slug"], name: "index_pages_on_slug", unique: true
 
 end
