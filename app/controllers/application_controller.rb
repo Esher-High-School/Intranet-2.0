c5b326d6e06aa0b006964be518c99f4c2b8e55b0
Joel Nichols <me@joelnichols.co.uk>
2015-05-18 12:33:59 +0100

    Subpage navigation now works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_pages
+  before_action :set_pages, :set_version
 
   private
+    def set_version
+      @version = `git rev-list HEAD --count`
+    end
     def set_pages
       @pages = Page.all.order('title ASC')
     end
