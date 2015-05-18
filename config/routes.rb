82907a27b429d80e6c2113ff401e4f8b5cc71db4
Joel Nichols <me@joelnichols.co.uk>
2015-05-18 11:44:59 +0100

    Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get '/' => 'static#index'
   resources :pages
   # The priority is based upon order of creation: first created -> highest priority.
