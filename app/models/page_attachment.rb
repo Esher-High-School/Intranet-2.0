2a18b68f138ce78d906fa68bad71262b215ea36e
Joel Nichols <me@joelnichols.co.uk>
2015-05-19 10:04:09 +0100

    Page attachments: Model now validates title, now has order

--- app/models/page_attachment.rb
+++ app/models/page_attachment.rb
@@ -1,6 +1,9 @@
 class PageAttachment < ActiveRecord::Base
   belongs_to :page
 
+  validates :title, presence: true
+  order 'title ASC'
+
   has_attached_file :attachment, :default_url => "/file/"
   do_not_validate_attachment_file_type :attachment
 end
