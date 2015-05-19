993cfcef04fa84111e90fc0ca4ca2e7d0e04f746
Joel Nichols <me@joelnichols.co.uk>
2015-05-19 10:10:52 +0100

    Fixed page attachment order

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,7 +4,7 @@ class Page < ActiveRecord::Base
   friendly_id :title, use: :slugged
   order 'title ASC'
 
-  has_many :page_attachments -> { order 'title ASC' },
+  has_many :page_attachments, -> { order 'title ASC' },
          dependent: :destroy
 
   has_many :children, -> { order 'title ASC' },
