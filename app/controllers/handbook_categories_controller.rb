6bcf91f3c0acb9259a5753c5168e15956e96e86c
Joel Nichols <me@joelnichols.co.uk>
2015-05-20 09:43:22 +0100

    Possible handbook fix

--- app/controllers/handbook_categories_controller.rb
+++ app/controllers/handbook_categories_controller.rb
@@ -11,7 +11,6 @@ class HandbookCategoriesController < ApplicationController
 
   def show
     @handbook_categories = HandbookCategory.all.order('title ASC')
-    redirect_to handbook_category_handbook_documents_path(@handbook_category)
   end
 
   def new
